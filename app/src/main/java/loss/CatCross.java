ea163e27a7607bee14f75af60059c387f82584bd
satdarn <satdarn@gmail.com>
2024-12-27 15:30:47 -0800

    Update CatCross.java

--- app/src/main/java/loss/CatCross.java
+++ app/src/main/java/loss/CatCross.java
@@ -1,36 +1,36 @@
 package loss;
 
-import loss.Loss;
 import matrix.Vector;
 
 public class CatCross implements Loss {
     @Override
     public Vector loss(Vector predicted, Vector actual) {
         Vector output = new Vector(actual.getRows(), 0);
-        for(int i = 0; i < actual.getRows(); i++){
-            double out = loss(predicted.get(i), actual.get(i));
+        for (int i = 0; i < actual.getRows(); i++) {
+            double out = -actual.get(i) * Math.log(predicted.get(i)); // Applying the cross-entropy formula
             output.set(i, out);
         }
-        
         return output;
     }
+
     @Override
     public Vector lossPrime(Vector predicted, Vector actual) {
         Vector output = new Vector(actual.getRows(), 0);
-        for(int i = 0; i < actual.getRows(); i++){
-            double out = lossPrime(predicted.get(i), actual.get(i));
+        for (int i = 0; i < actual.getRows(); i++) {
+            // Derivative of categorical cross-entropy: p - y
+            double out = predicted.get(i) - actual.get(i);
             output.set(i, out);
         }
         return output;
     }
-    
+
     @Override
     public double loss(double predicted, double actual) {
-        return 0;
+        return -actual * Math.log(predicted); // Cross-entropy for a single prediction
     }
-    
+
     @Override
     public double lossPrime(double predicted, double actual) {
-        return 0;
+        return predicted - actual; // Derivative for a single prediction
     }
 }
