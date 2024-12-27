a11d802f2135892d15e9ddb071600169cdc72e91
satdarn <satdarn@gmail.com>
2024-12-27 12:49:24 -0800

    Add activation documentation

--- app/src/main/java/layer/activation/Activation.java
+++ app/src/main/java/layer/activation/Activation.java
@@ -4,11 +4,27 @@ import layer.Layer;
 import matrix.Vector;
 import matrix.Matrix;
 
+/**
+ * This class represents an abstract activation function for a layer in a neural
+ * network
+ * 
+ * @author Joseph Bronsten
+ */
 public abstract class Activation extends Layer{
 
+    /**
+     * Create an activation function with a given input
+     * @param inputSize the given input
+     */
     public Activation(int inputSize){
         super(inputSize, inputSize);
     }
+
+    /**
+     * The forward propegation through the layer
+     * @param input the input to the forward prop
+     * @return the result of the forward prop
+     */
     public Vector forwardProp(Vector input){
         setInputVector(input);
         for(int index = 0; index < input.getRows(); index++){
@@ -18,6 +34,12 @@ public abstract class Activation extends Layer{
         }
         return input;
     }
+
+    /**
+     * The backpropagation through the layer
+     * @param outputGrad the gradient of the output layer
+     * @return the gradient of the input layer
+     */
     public Vector backProp(Vector outputGrad){
         Vector inputGrad = new Vector(getInputVector().getRows(), 0);
         for(int index = 0; index < getInputVector().getRows(); index++){
@@ -28,6 +50,17 @@ public abstract class Activation extends Layer{
         inputGrad = (Vector) Matrix.multiply(outputGrad, inputGrad);
         return inputGrad;
     }
+    /**
+     * The base activation function
+     * @param input the input of the function
+     * @return the output of the function
+     */
     protected abstract double activationFunc(double input);
+
+    /**
+     * The derivative of the base activation function
+     * @param input the input of the function
+     * @return the derivative of the function
+     */
     protected abstract double activationPrime(double input);
 }
