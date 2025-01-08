ccf78cc9476414f0d595acf37f3342f50a69151d
satdarn <satdarn@gmail.com>
2025-01-07 17:22:20 -0800

    Update Loss.java

--- app/src/main/java/loss/Loss.java
+++ app/src/main/java/loss/Loss.java
@@ -3,8 +3,6 @@ package loss;
 import matrix.Vector;
 
 public interface Loss {
-    double loss(double predicted, double actual);
-    Vector loss(Vector predicted, Vector actual);
-    double lossPrime(double predicted, double actual);
+    double loss(Vector predicted, Vector actual);
     Vector lossPrime(Vector predicted, Vector actual);
 }
